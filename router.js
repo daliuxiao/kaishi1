6cb1c357dbb4527c2420fa2948568d45fa3dc60f
daliuxiao <420448184@qq.com>
2018-01-20 17:21:20 +0800

    第一次提交

---
+++ router.js
@@ -0,0 +1,48 @@
+Vue.config.productionTip = true
+var Home = {
+	template: `
+		<div>
+			<div style="width: 400px;height: 500px;background-color: brown;position: absolute;top: 60px;left: 270px;">文章列表</div>
+			<div style="width: 400px;height: 500px;background-color: blue;position: absolute;top: 60px;left: 690px;">视频列表</div>
+		</div>
+`
+}
+var Event = {
+	template: `
+		<div style="width: 1000px;margin:auto;">
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 100px;text-align:center;line-height:60px">文章列表</div>
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 200px;text-align:center;line-height:60px">视频列表</div>
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 300px;text-align:center;line-height:60px">视频列表</div>
+		</div>
+`
+}
+var Video = {
+	template: `
+		<div style="width: 1000px;margin:auto;">
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 100px;text-align:center;line-height:60px">广告栏</div>
+			<div style="width: 1000px;height: 500px;background-color: dodgerblue;position: absolute;top: 200px;text-align:center;line-height:60px">视频内容</div>
+		</div>
+`
+}
+
+var routes = [{
+		path: "/",
+		component: Home,
+},
+	{
+		path: "/events",
+		component: Event,
+},
+	{
+		path: "/videos",
+		component: Video,
+}]
+//新建vuerouter对象，将router关系设置
+var router = new VueRouter({
+	routes
+})
+//定义vue实例
+var vm = new Vue({
+	el: "#app",
+	router
+})
